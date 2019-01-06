1d9f42e768b853f8fe03deb29f8b3fbb64613b34
Zachary Reece <zachdreece@gmail.com>
2019-01-05 18:42:37 -0600

    Tested and Working

---
+++ src/components/streams/StreamCreate.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class StreamCreate extends Component {
+  render() {
+    return <div>StreamCreate</div>;
+  }
+}
---
+++ src/components/streams/StreamShow.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class StreamShow extends Component {
+  render() {
+    return <div>StreamShow</div>;
+  }
+}
